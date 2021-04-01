5b073d6d26bff548f80ded37394eb99f737afc45
dohuruogu123 <47153027+dohuruogu123@users.noreply.github.com>
2021-04-01 12:06:25 +0100

    initial app set up, initial database + model set up

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
-from flask import Flask 
+from flask import Flask
+from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+app = Flask(__name__)
+app.config.from_object(Config)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///../data/books_and_authors.sqlite'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+from models import Users, Product, Category, Search_result, Category
+
+
+
